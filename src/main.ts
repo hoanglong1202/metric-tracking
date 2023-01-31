55ef0b4474905393e5ef6d217b210106d889fe3f
Accel <cyberpunk2076@gmail.com>
2023-01-31 23:10:29 +0700

    change port

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/documents', app, document);
 
-  await app.listen(5000);
+  await app.listen(3000);
 }
 bootstrap();
